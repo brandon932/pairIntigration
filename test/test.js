4ad7b9e035fda2aff8d71276a50d3cdabe5317a1
brandon932 <bthornton_932@yahoo.com>
2015-10-16 10:46:27 -0600

    added travis and tests for science

---
+++ test/test.js
@@ -0,0 +1,25 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+
+var server = require('../server/app');
+
+var should = chai.should();
+chai.use(chaiHttp);
+describe('Blobs', function() {
+  it('return 200 on / GET', function(done) {
+    chai.request(server)
+    .get('/')
+    .end(function(err, res){
+      res.should.have.status(200);
+      done();
+    });
+  });
+  it('should fail on expected 404 / GET', function(done) {
+    chai.request(server)
+    .get('/')
+    .end(function(err, res){
+      res.should.have.status(404);
+      done();
+    });
+  });
+});
